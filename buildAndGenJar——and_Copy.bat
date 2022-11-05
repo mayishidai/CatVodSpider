1c711c767c8e33f6076958b0673effbaa4ddc1e5
mayishidai <1620829248@qq.com>
2022-11-05 23:17:59 +0800

    摞动到api下

--- buildAndGenJar——and_Copy.bat
+++ buildAndGenJar——and_Copy.bat
@@ -4,9 +4,9 @@ call "%~dp0\gradlew" assembleRelease --no-daemon
 
 call "%~dp0\jar\genJar.bat" %1
 
-copy "%~dp0\jar\custom_spider.jar" "D:\PythonProject\PythonProjects\projects\tvbox\custom_spider.jar"
+copy "%~dp0\jar\custom_spider.jar" "D:\PythonProject\PythonProjects\projects\tvbox\api\custom_spider.jar"
 copy "%~dp0\jar\custom_spider.jar" "D:\PythonProject\PythonProjects\projects\tvbox\custom\spider\custom_spider.jar"
-copy "%~dp0\jar\custom_spider.jar" "D:\PythonProject\PythonProjects\projects\tvbox\custom_spider_debug.jar"
+copy "%~dp0\jar\custom_spider.jar" "D:\PythonProject\PythonProjects\projects\tvbox\api\custom_spider_debug.jar"
 copy "%~dp0\jar\custom_spider.jar" "D:\PythonProject\PythonProjects\projects\tvbox\custom\spider\custom_spider_debug.jar"
 
 pause
